196302cfd08a32844851a9ead590f4731b4b9f79
Mike Wagner <mvw5669@psu.edu>
2022-02-06 23:50:54 -0500

    final

--- src/NasaImageSearch.js
+++ src/NasaImageSearch.js
@@ -10,7 +10,6 @@ export class NasaImageSearch extends LitElement {
   constructor() {
     super();
     this.returnDataOnly = false;
-    this.getData();
     this.arrayOfData = []; // Stores image search query data
     this.searchWords = "Moon Landing";
     this.checked = false;
@@ -20,9 +19,9 @@ export class NasaImageSearch extends LitElement {
   static get properties() {
     return {
       returnDataOnly: { type: Boolean },
-      searchWords: { type: String },
+      searchWords: { type: String, reflect: true },
       checked: { type: Boolean, reflect: true },
-
+      searched: { type: Boolean, reflect: true },
       arrayOfData: { type: Array },
     };
   }
@@ -31,17 +30,24 @@ export class NasaImageSearch extends LitElement {
   // this allows you to react to variables changing and use javascript to perform logic
   updated(changedProperties) {
     changedProperties.forEach((oldValue, propName) => {
-      // if (propName === 'view') {
-      //   if (this.view === 'card') {
-      //     console.log('card!');
-      //   }
-      // }
+      if (propName === "checked") {
+        if (this.checked) {
+          console.log('checked!');
+        }
+      }
+
+      if (propName === "searched") {
+        if (this.searched) {
+          this.getData();
+          this.searched = !this.searched;
+        }
+      }
     });
   }
 
   getData() {
     // defined
-    const file = new URL(
+    let file = new URL(
       `https://images-api.nasa.gov/search?q=${this.searchWords}&media_type=image`
     );
     // go get our data from Nasa file
@@ -58,7 +64,11 @@ export class NasaImageSearch extends LitElement {
         this.arrayOfData = [];
         data.collection.items.forEach(item => {
           let imageObj = {}; // declare object and set to empty
-          const { href } = item; // grabs link from href before heading into item child
+          let href; // grabs link from href before heading into item child
+
+          item.links.forEach(information => {
+            href = `${information.href}`;
+          });
 
           item.data.forEach(info => {
             // info = "0" in a Nasa API
@@ -68,13 +78,11 @@ export class NasaImageSearch extends LitElement {
               href: `${href}`,
               title: `${info.title}`,
               description: `${info.description}`,
-              photographer: `${info.photographer}`,
+              secondary_creator: `${info.secondary_creator}`,
             };
           });
           this.arrayOfData.push(imageObj); // push objects to array
         });
-
-        console.log(this.arrayOfData);
       });
   }
 
@@ -98,8 +106,16 @@ export class NasaImageSearch extends LitElement {
     super.disconnectedCallback();
   }
 
-  __clicked() {
+  __checkClicked() {
     this.checked = !this.checked;
+  }
+
+  __search() {
+    this.searched = !this.searched;
+  }
+
+  __searchChange(value) {
+    this.searchWords = value;
   }
 
   // CSS - specific to Lit
@@ -119,29 +135,42 @@ export class NasaImageSearch extends LitElement {
   render() {
     return html`
       <div class="searchArea">
-        <input type="text" value="${this.searchWords}">
-        <button class="searchButton">Search</button>
+        <input type="text" id="searchTextBox" value="${this.searchWords}" @change=${e => this.__searchChange(e.target.value)}>
+        <button class="searchButton" @click="${this.__search}">Search</button>
         <label for="dataCheck">Return Data Only</label>
-        <input type="checkbox" id="dataCheck" .checked="${this.checked}" @click="${this.__clicked}">
+        <input type="checkbox" id="dataCheck" .checked="${this.checked}" @click="${this.__checkClicked}">
       </div>
       <div class="searchReturnArea">
         ${this.checked
-          ? html` <ul class="listArea"></ul> `
+          ? html` 
+            <ul class="listArea">
+              ${this.arrayOfData.map(
+                item => html`
+                  <li>
+                    Link: ${item.href}<br>Title: ${item.title}<br>Description: ${item.description}<br>Secondary Creator: ${item.secondary_creator}
+                  </li>
+                  <br>
+                `
+              )}
+            </ul>
+          `
           : html`
+            ${this.arrayOfData.map(
+              item => html`
               <accent-card
-                image-src="https://webcomponents.psu.edu/styleguide/elements/accent-card/demo/images/image7.jpg"
+                image-src="${item.href}"
                 accent-color="green"
                 horizontal
-                style="max-width:600px;"
+                style="width:auto;"
               >
-                <div slot="heading">Magna Donec</div>
+                <div slot="heading">${item.title}</div>
                 <div slot="content">
-                  Luctus vel iaculis orci porttitor. Blandit semper phasellus eu
-                  nunc viverra vitae. Tincidunt amet quis volutpat fusce? Donec
-                  lobortis id mauris nisl nec et euismod. Purus purus accumsan
-                  at.
+                  ${item.description} - ${item.secondary_creator}
                 </div>
               </accent-card>
+              `
+            )}
+              
             `}
       </div>
     `;
